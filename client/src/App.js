7284eab673f2ca0f967a33a49d1514bd68f843ec
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-01-13 18:09:51 -0500

    Added two routes for MovieList and Movie component respectively

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route, Switch } from 'react-router-dom';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 
 import SavedList from './Movies/SavedList';
 
@@ -14,13 +17,14 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
         });
     }
     getMovies();
-  }, []);
+  }, [ ]);
 
   const addToSavedList = id => {
     // This is stretch. Prevent the same movie from being "saved" more than once
@@ -30,7 +34,14 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Switch>
+        <Route path={"/movies/:id"}>
+            <Movie/>
+        </Route>
+        <Route path={"/"}>
+            <MovieList movies={movieList}/>
+        </Route>
+      </Switch>
     </div>
   );
 }
